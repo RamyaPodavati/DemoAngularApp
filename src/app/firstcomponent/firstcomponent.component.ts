7b2e5a3c1be5ed995630074bbad55adbcdb1f27a
RamyaPodavati <ramya.podavati.sai@gmail.com>
2023-02-14 18:00:22 -0600

    TestCommit

---
+++ src/app/firstcomponent/firstcomponent.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firstcomponent',
+  templateUrl: './firstcomponent.component.html',
+  styleUrls: ['./firstcomponent.component.css']
+})
+export class FirstcomponentComponent{
+      Title: string = '';
+ngModel: any;
+      constructor(){}
+
+      onValueChange(){
+
+
+      }
+
+}
